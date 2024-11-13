ba7fc4d708610edd071bab0066ccaca73e84e615
Matheus Alves <matheusrike.alves@gmail.com>
2024-11-13 17:00:43 -0300

    Iniciado o desenvolvimento da sidebar da aplicação.

--- src/app/app.php
+++ src/app/app.php
@@ -4,15 +4,39 @@
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dolt App</title>
     <link rel="/src/global/img/Favicon.ico" href="favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" media="screen" href="/src/global/global.css">
-    <link rel="stylesheet" type="text/css" media="screen" href="/src/app/assets/css/style.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="../global/global.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="./assets/css/style.css">
+
+    <!-- Materialize CDN links -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </head>
+
 <body>
-    <h1>Teste</h1>
+    <div class="sidebar">
+        <header>
+            <img src="./assets/img/profile-icon.svg" alt="" height="50px">
+            <?php
+            echo '<p>Nome de usuário</p>';
+            ?>
+        </header>
+        <hr>
+        <div class="container">
+            <div class="alert"></div>
+            <div class="newGroup"></div>
+        </div>
+        <hr>
+        <footer>
+            <img src="../global/img/Logo.svg" alt="Logo da Dolt" height="30px">
+            <button><img src="./assets/img/logout-icon.svg" alt="Sair" height="20px"></button>
+        </footer>
+    </div>
 </body>
+
 </html>
