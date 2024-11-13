9c6f9ab5009c3981d39a930b730c172f07727dc6
Matheus Alves <matheusrike.alves@gmail.com>
2024-11-13 14:11:56 -0300

    Configuração básica do projeto.

---
+++ src/app/app.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dolt App</title>
+    <link rel="/src/global/img/Favicon.ico" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" media="screen" href="/src/global/global.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="/src/app/assets/css/style.css">
+</head>
+<body>
+    <h1>Teste</h1>
+</body>
+</html>
