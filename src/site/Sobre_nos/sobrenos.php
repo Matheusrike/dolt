212eb65039af2e8bee2028bf2ba2f104aaa10d56
Houzeeer <matheus.mastroumano04@gmail.com>
2024-11-19 17:17:35 -0300

    Desenvolvimento dos sites

---
+++ src/site/Sobre_nos/sobrenos.php
@@ -0,0 +1,75 @@
+ <?php
+include 'arrays.php';
+ ?> 
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="sobrenos.css">
+</head>
+
+<body>
+    <?php 
+    renderheader();
+    ?>
+
+    <section class="initial-section">
+        <div class="content-container">
+            <img src="img/imagem_principal.svg" class="img-inicial">
+            <div class="text-container">
+                <p class="initial-text">Conheça-nos</p>
+                <p class="initial-bigtext">Uma Cultura Feita para Conectar, Inovar e Crescer</p>
+                <p class="initial-subtext">Na Dolt, acreditamos que boas ideias surgem em um ambiente que valoriza
+                    diversidade, teamwork e aprendizado. Cada membro da equipe é incentivado a crescer e colaborar na
+                    transformação de desafios em soluções. Junte-se a nós!</p>
+                <div class="initial-btndiv">
+                    <button class="btn-saibamais">Saiba Mais</button>
+                    <button class="btn-nossoprojeto">Nosso Projeto</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="missao-section">
+        <div class="missao-minicont">
+            <img src="img/alvo.svg" class="img-missaomini">
+            <p class="missao-text">Nossa missão é transformar a gestão de tarefas em uma experiência simples.</p>
+            <p class="missao-subtext">Nosso objetivo é otimizar a organização e a produtividade nas rotinas diárias. Uma
+                gestão eficaz de tarefas é fundamental para o sucesso de projetos, garantindo resultados melhores e
+                desenvolvimento pessoal.</p>
+        </div>
+        <img src="img/imagem1.svg" class="img-missao">
+    </section>
+
+    <section class="visao-section">
+        <img src="img/imagem2.svg" class="img-visao">
+        <div class="visao-minicont">
+            <img src="img/Binoculos.svg" class="img-visaomini">
+            <p class="visao-text">Nossa visão é transformar a gestão de tarefas em uma experiência simples e eficaz.</p>
+            <p class="visao-subtext">Acreditamos que a organização é a chave para a produtividade. Nossa missão é
+                oferecer uma plataforma que facilite o gerenciamento de tarefas e ajude as pessoas a alcançar seus
+                objetivos com eficiência.</p>
+        </div>
+    </section>
+
+    <section class="valores-section">
+        <div class="valores-minicont">
+            <img src="img/valores.svg" class="img-valores">
+            <p class="valores-text"></p>
+            <p class="valores-subtext"></p>
+        </div>
+        <div class="valores-card">
+            <img src="img/.svg" class="img-valoresmini">
+            <p class="valores-cardtext"></p>
+            <p class="valores-cardsubtext"></p>
+        </div>
+    </section>
+
+
+</body>
+
+</html>
