a02c5817e64cfdf382a581c4849e3be6ecc3f162
Matheus Alves <matheusrike.alves@gmail.com>
2024-11-19 11:23:15 -0300

    Save

--- src/backend/functions/user_registration.php
+++ src/backend/functions/user_registration.php
@@ -1,22 +1,58 @@
 <?php
 
+/* ------------ Função que gera um ID único para um novo usuário ------------ */
+function generate_user_id()
+{
 
-$users = json_decode(file_get_contents('../data/users_data.json'), true);
-print_r($users);
+    // Pega o arquivo JSON e transforma em um array com o mesmo nome do arquivo;
+    $users_data = json_decode(file_get_contents('../data/users_data.json'), true);
 
-function generate_user_id() {
+    //Acessa o array users que está dentro do arquivo users_data.json 
+    $users = $users_data['users'];
+
+    //Define uma variável max_id e atribui o valor 0
     $max_id = 0;
+
+    //Acessa o array users e denomina cada objeto como $user
     foreach ($users as $user) {
-        if ($user['id'] > $max_id) {
-            $max_id = $user['id'];
+
+        // Verifica se o valor da chave "user_id" do objeto $user é nulo ou maior ou igual ao $max_id.
+        if ($user['user_id'] == null || $user['user_id'] >= $max_id) {
+
+            // Até que a condição seja verdadeira, o $max_id recebe o valor da chave "user_id" do objeto $user da vez.
+            $max_id = $user['user_id'];
         }
     }
-    $max_id++;
+
+    //Após isso o $max_id vai ser igual ao id do ultimo objeto $user do array users
+
+    //Então a função retorna o valor do $max_id + 1, para ser gerado o proximo id do usuário;
+    return $max_id + 1;
 }
 
-function register_user($username, $email, $password) {
+/* ------- Função que cria um novo usuário no arquivo users_data.json ------- */
+function register_user($username, $email, $password)
+{
     $user_id = generate_user_id();
-    $user_username = $_POST['username'];
-    $user_email = $_POST['email'];
-    $user_password = $_POST['password'];
+    $user_username = $username;
+    $user_email = $email;
+    $user_password = $password;
+
+    $data = json_decode(file_get_contents('../data/users_data.json'), true);
+
+    $data['users'][] = [
+        'user_id' => $user_id,
+        'username' => $user_username,
+        'user_email' => $user_email,
+        'user_password' => $user_password
+    ];
+
+    file_put_contents('../data/users_data.json', json_encode($data));
 }
+
+/* ----------------------- Teste de inserção de dados ----------------------- */
+register_user('Jorginho', 'Jorginho@email.com', 'Coxinha_123');
+$users = json_decode(file_get_contents('../data/users_data.json'), true);
+print '<pre>';
+print_r($users);
+print '</pre>';
